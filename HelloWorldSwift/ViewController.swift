fc1bb4e507c6725cfc84631978c403e2985bbaec
Tanushree Chaubal <tanushree2492@gmail.com>
2016-08-19 22:00:31 -0700

    Introductory iOS Swift app
    Basic Hello World app

--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWorldSwift(nameTextField: UITextField) {
+        nameLabel.text = "Hello \(nameTextField.text)"
+    }
 
 }
 
